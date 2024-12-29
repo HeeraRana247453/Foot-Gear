ea07860ebb76191461b671cf55f259ff74bb13af
Heera Rana <heerarana526@gmail.com>
2024-12-29 16:57:04 +0530

    Changed vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Nike/', // Replace with your repository name if different
+  plugins: [react()]
 })
